5ae79486a4c3f5655da58d9e5e3a979a44870803
Gabriel67OFC <bielzinhodograuofc@gmail.com>
2026-05-13 17:35:13 -0300

    Primeira sobre condicional

---
+++ programa02.py
@@ -0,0 +1,19 @@
+# Atividade de média
+
+frequencia = int(input("Informe quantos dias o aluno compareceu as aulas: "))
+
+if frequencia > 0:
+    print("Não reprovou por falta")
+
+    n1 = float(input("Digite a primeita nota").replace(",", "."))
+    n2 = float(input("Digite a segunda nota").replace (",", "."))
+
+    media = (n1 + n2)/2
+    if media>=7: 
+        print("aprovado")
+    elif media>=5:
+        print("Ficou de recuperacao")
+else: 
+    print("Reprovado por falta")
+
+    
