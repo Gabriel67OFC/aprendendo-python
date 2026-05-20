7a4c0b0a798fb034f680133907cecaa7d292b19d
Gabriel67OFC <bielzinhodograuofc@gmail.com>
2026-05-20 17:25:11 -0300

    Atividade True ou False

---
+++ Exercicio True ou False.py
@@ -0,0 +1,30 @@
+
+exerc1 = (15>7) and (3==3)
+print(f"O resultado da primeira é {exerc1}")
+
+exerc2 = (10<4) or (8!=5)
+print(f"O resultado da segunda é {exerc2}")
+
+exerc3 = not (20>=20)
+print(f"O resultado da terceira é {exerc3}")
+
+exerc4 = (5*2==10) and not (3>4)
+print(f"O resultado da quarta é {exerc4}")
+
+exerc5 = (12/2==5) or (7<=7)
+print(f"O resultado da quarta é {exerc5}")
+
+exerc6 = not (9!=9) and (4>1)
+print(f"O resultado da quarta é {exerc6}")
+
+exerc7 = (5*2==10) and not (3>4)
+print(f"O resultado da quarta é {exerc7}")
+
+exerc8 = not((18<20) and (5!=5))
+print(f"O resultado da quarta é {exerc8}")
+
+exerc9 = (0==1) or not((3>=2) and (4<=4))
+print (f"O resultado da quarta é {exerc9}")
+
+exerc10 = ((10>5) and not(2==3)) or (8<4)
+print(f"O resultado da quarta é {exerc10}")
