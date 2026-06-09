2ac5dda8a394715766b8e3fcf0e7cc34ce7e16b6
Gabriel67OFC <bielzinhodograuofc@gmail.com>
2026-06-09 17:34:52 -0300

    Atividade dia 09.06.2026

---
+++ matriz/BatalhaNaval.py
@@ -0,0 +1,22 @@
+print("------------------------------------------------")
+print("BATALHA NAVAL COM AURA INFINITA MAIS EGO SUPREMO")
+print("------------------------------------------------")
+
+
+
+oceano = [
+    ['~', '~', '~', '~'],
+    ['~', '~', '~', '~'],
+    ['~', '~', '~', '~'],
+    ['~', '~', '~', '~']
+]
+
+oceano[2][3]="N"
+
+linha = int(input("FALA A LINHA AÍ BOCA DE SACOLA KAKAKAKA: ;)"))
+coluna = int(input("FALA A COLUNA TAMBÉM, BOCA DE BODE KAKAKAKAKA: "))
+
+if oceano [linha][coluna] == "N":
+        print("Boa, você acertou a canoa com T-T")
+else:
+     print("VOCÊ ERROU BESTÃO!!!! DESISTA DOS SEUS SONHOS E MORRA MUAHAHAHAHHAHAH")
